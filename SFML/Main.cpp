2c847ce8b1763b8fb27076b72daf4b8ad283c7d2
levinhthuanle <thuanle.qt@gmail.com>
2024-03-12 10:59:11 +0700

    feat: Change the structure of the project

--- SFML/Main.cpp
+++ SFML/Main.cpp
@@ -3,34 +3,6 @@ int main()
 {
     Drawio drawio;
 
-    // Generate the main window
-    sf::RenderWindow window(sf::VideoMode(1700, 950), "SFML Tutorial", sf::Style::Close | sf::Style::Titlebar);
-    std::cout << "Generate the main window success" << std::endl;
-
-    // Main loop
-    while (window.isOpen()) {
-        sf::Event event;
-
-        while (window.pollEvent(event))
-        {
-            switch (event.type)
-            {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::TextEntered:
-                if (event.text.unicode < 128) {
-                    printf("%c", event.text.unicode);
-                }
-                break;
-            default:
-                break;
-            }
-        }
-
-        window.clear(); // Clear the window first
-        window.display(); // Display the window
-    }
 
     return 0;
 }
--- SFML/Window.cpp
+++ SFML/Window.cpp
@@ -1,1 +1,35 @@
 #include "Window.h"
+
+void Window::initLoginWindow()
+{    
+    // Generate the main window
+    sf::RenderWindow window(sf::VideoMode(1700, 950), "SFML Tutorial", sf::Style::Close | sf::Style::Titlebar);
+    std::cout << "Generate the main window success" << std::endl;
+
+    // Main loop
+    while (window.isOpen()) {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+
+            case sf::Event::TextEntered:
+                if (event.text.unicode < 128) {
+                    printf("%c", event.text.unicode);
+                }
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        window.clear(); // Clear the window first
+        window.display(); // Display the window
+    }
+}
