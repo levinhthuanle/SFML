65b8e88d25835d5e8c8f5e3544a7a84f8f74311f
Tu Thanh <tuthanh10825@gmail.com>
2024-03-15 10:52:31 +0700

    adding function to check validity when login in "extra function.h"

--- SFML/Teacher.h
+++ SFML/Teacher.h
@@ -1,4 +1,17 @@
 #pragma once
+#include <string>
+#include <filesystem>
+namespace fsys = std::filesystem; 
 class Teacher
 {
+
+	void create_sy(std::string name)
+	{
+		if (!fsys::exists("data/student" + name)) return; 
+		fsys::create_directory("data/student" + name);
+	}
+	void create_student()
+	{
+
+	}
 };
