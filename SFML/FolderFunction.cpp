e4a16014c938718aa8982a373933c76dc39dfc3f
Tu Thanh <tuthanh10825@gmail.com>
2024-03-16 14:34:31 +0700

    change create_directory -> create_directories.

--- SFML/FolderFunction.cpp
+++ SFML/FolderFunction.cpp
@@ -1,11 +1,12 @@
 #include "FileNFolder.h"
 
-void create_multiple_directories(fsys::path path, Stack <std::string> list_name) //TODO: Create multiple directories based on the linked list which a teacher input for classes in school year.
+void create_multiple_directories(fsys::path path, Stack <std::string> list_name) 
+//Create multiple directories based on the linked list which a teacher input for classes in school year // student in a class.
 {
 	Stack<std::string>::ListNode* curr = list_name.head; 
 	while (curr)
 	{
-		fsys::create_directory(path/curr->val); 
+		fsys::create_directories(path/curr->val); 
 		curr = curr -> next; 
 	}
 }
