45edc2f6fe3b584506401cc36a9fd450af6a62d7
cuongle090102 <116471753+cuongle090102@users.noreply.github.com>
2024-03-28 16:42:02 +0700

    Update ShoolYear and create Class object

---
+++ SFML/Class.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Requirement.h"
+
+class Class {
+private:
+    std:string classID, className;
+    Stack<Student> students;
+public:
+    Class(std::string classID, std::string className) {
+		this->classID = classID;
+		this->className = className;
+	}
+	void addStudent(Student student) {
+		students.push(student);
+	}
+	std::string getClassID() {
+		return classID;
+	}
+	std::string getClassName() {
+		return className;
+	}
+	Stack<Student> getStudents() {
+		return students;
+	}
+	void setClassID(std::string classID) {
+		this->classID = classID;
+	}
+	void setClassName(std::string className) {
+		this->className = className;
+	}
+	~Class() {
+		while (!students.isEmpty()) {
+			students.pop();
+		}
+	})
+};
