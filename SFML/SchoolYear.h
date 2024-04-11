292dd12d6bbff597082fd0a78bc2ef5d3194ec72
Thuanle27 <thuanle.qt@gmail.com>
2024-04-11 17:39:38 +0700

    feat: rewrite schoolyear and semester

--- SFML/SchoolYear.h
+++ SFML/SchoolYear.h
@@ -1,69 +1,39 @@
-//#pragma once
-//#include "Requirement.h"
-//
-//class SchoolYear {
-//private:
-//	std::string year;
-//	Stack<Semester> semesters;
-//	Stack<Class> classes;
-//public:
-//	SchoolYear(std::string year) : year(year) {
-//
-//		std::string directory = "data/" + year;
-//
-//		// for (const auto& entry : std::filesystem::directory_iterator(directory)) {
-//		//     semesters.push(Semester(entry.path()));
-//		// }
-//	}
-//	~SchoolYear() {
-//		while (!semesters.isEmpty()) {
-//			semesters.pop();
-//		}
-//	}
-//	//Semester
-//	void addSemester(Semester semester) { semesters.push(semester); }
-//
-//	void removeSemester() { semesters.pop(); }
-//
-//
-//
-//	Semester* getLastedSemester() {
-//		return semesters.top();
-//	}
-//
-//
-//	bool inputNewSemester() {
-//		std::string semesterName;
-//		std::cout << "Enter semester name: ";
-//		std::cin >> semesterName;
-//		Semester semester(semesterName);
-//		semesters.push(semester);
-//		return true;
-//	}
-//
-//	//Class
-//	void addClass(Class newClass) {
-//		classes.push(newClass);
-//	}
-//
-//	Class* getClass() {
-//		return classes.top();
-//	}
-//
-//	void removeClass() { classes.pop(); }
-//	bool inputNewClass() {
-//		std::string className;
-//		std::cout << "Enter class name: ";
-//		std::cin >> className;
-//		Class newClass(className);
-//		classes.push(newClass);
-//		return true;
-//	}
-//};
-//#include "Requirement.h"
-//#include "BuiltClasses.h"
-//
-
-
 #pragma once
-//TODO: finish student first, than to school year. 
+#include "Requirement.h"
+#include "Semester.h"
+#include "Class.h"
+class SchoolYear {
+private:
+	std::string year;
+	vector<Semester> semesters;
+	vector<Class> classes;
+public:
+	SchoolYear() {}
+
+	std::string getYear() {
+		return year;
+	}
+
+	vector<Semester> getSemester() {
+		return semesters;
+	}
+
+	vector<Class> getClasses() {
+		return classes;
+	}
+
+	void setYear(std::string year) {
+		this->year = year;
+	}
+
+	void setSemester(vector<Semester> semesters) {
+		this->semesters = semesters;
+	}
+
+	void setClasses(vector<Class> classes) {
+		this->classes = classes;
+	}
+
+
+};
+
--- SFML/Semester.h
+++ SFML/Semester.h
@@ -1,75 +1,67 @@
+#pragma once
 #include "Requirement.h"
-//
-//class Semester {
-//private:
-//	std::string name;
-//	Date startDate;
-//	Date endDate;
-//	Stack<Course> courses;
-//public:
-	//Semester(std::string directory) {
+#include "Date.h"
+#include "Vector.h"
+#include "Course.h"
+class Semester {
+private:
+	fsys::path semesterPath;
+	std::string name;
+	Date startDate;
+	Date endDate;
+	vector<Course> courses;
+public:
+	Semester(std::string directory) {
 
-	//	name = directory.substr(directory.find_last_of("/") + 1);
-	//	std::ifstream file(directory + "/start_end_time.txt");
-	//	std::string line;
-	//	std::getline(file, line);
-	//	startDate = Date(line);
-	//	std::getline(file, line);
-	//	endDate = Date(line);
+		name = directory.substr(directory.find_last_of("/") + 1);
+		std::ifstream file(directory + "/start_end_time.txt");
+		std::string line;
+		std::getline(file, line);
+		startDate = Date(line);
+		std::getline(file, line);
+		endDate = Date(line);
 
-	//	//for (const auto& entry : std::filesystem::directory_iterator(directory + "/courses")) {
-	//	//    courses.push(Course(entry.path()));
-	//	//}
-	//}
-	//~Semester() {
-	//	while (!courses.empty()) {
-	//		courses.pop();
-	//	}
-	//}
+		//for (const auto& entry : std::filesystem::directory_iterator(directory + "/courses")) {
+		//    courses.push(Course(entry.path()));
+		//}
+	}
+	~Semester() {
+		
+	}
 
-	//std::string getName() {
-	//	return name;
-	//}
+	std::string getName() {
+		return name;
+	}
 
-	//Date getStartDate() {
-	//	return startDate;
-	//}
+	Date getStartDate() {
+		return startDate;
+	}
 
-	//Date getEndDate() {
-	//	return endDate;
-	//}
+	Date getEndDate() {
+		return endDate;
+	}
 
-	//Stack<Course> getCourses() {
-	//	return courses;
-	//}
+	vector<Course> getCourses() {
+		return courses;
+	}
 
-	//void addCourse(Course course) {
-	//	courses.push(course);
-	//}
+	void addCourse(Course course) {
+		
+	}
 
-	//void removeCourse(Course course) {
-	//	Stack<Course> temp;
-	//	while (!courses.empty()) {
-	//		if (courses.top().getName() != course.getName()) {
-	//			temp.push(courses.top());
-	//		}
-	//		courses.pop();
-	//	}
-	//	while (!temp.empty()) {
-	//		courses.push(temp.top());
-	//		temp.pop();
-	//	}
-	//}
-	//void save() {
-	//    std::ofstream file("semesters/" + name + "/start_end_time.txt");
-	//    file << startDate.toString() << std::endl;
-	//    file << endDate.toString() << std::endl;
-	//    file.close();
-	//    std::filesystem::create_directory("semesters/" + name + "/courses");
-	//    while (!courses.empty()) {
-	//        courses.top().save();
-	//        courses.pop();
-	//    }
-	//}
+	void removeCourse(Course course) {
+		
+	}
+	void save() {
+	    //std::ofstream file("semesters/" + name + "/start_end_time.txt");
+	    //file << startDate.toString() << std::endl;
+	    //file << endDate.toString() << std::endl;
+	    //file.close();
+	    //std::filesystem::create_directory("semesters/" + name + "/courses");
+	    //while (!courses.empty()) {
+	    //    courses.top().save();
+	    //    courses.pop();
+	    //}
+	}
 
-//};
+};
